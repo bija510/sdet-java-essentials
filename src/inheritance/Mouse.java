48221d2aabbcc282dab2fb2d3f07178465d0190a
bija510 <error.zxz@gmail.com>
2020-06-27 23:31:00 -0400

    Added Access Modifier & Inheritance

---
+++ src/inheritance/Mouse.java
@@ -0,0 +1,20 @@
+package inheritance;
+
+public class Mouse {
+
+	public static void leftClick() {
+		System.out.println("Lclick!");
+	}
+	
+	public static void rightClick() {
+		System.out.println("Rclick!");
+	}
+
+	public static void scrollUp() {
+		System.out.println("Scrolled Up!");
+	}
+	
+	public static void scrolldown() {
+		System.out.println("Scrolled Down!");
+	}
+}
